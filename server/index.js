778dd3793f7b46f7e739c57172b368b947144326
yoneda <nsshowdream@gmail.com>
2020-03-04 13:24:26 +0900

    [add] add babel to use es6 modules

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const nextApp = next({ dev, dir: "./client" });
 const handle = nextApp.getRequestHandler();
 
 const db = require("./db");
-const asyncHandler = require('express-async-handler');
+const asyncHandler = require("express-async-handler");
+
+import getUserData from "./util/getUser";
 
 nextApp
   .prepare()
@@ -23,10 +25,40 @@ nextApp
     });
 
     // async/await 構文を用いた遅延処理
-    server.get("/api/users", asyncHandler(async(req, res, next) => {
-      const users = await db.select().from("users");
-      res.send(users);
-    }));
+    server.get(
+      "/api/users",
+      asyncHandler(async (req, res, next) => {
+        // const users = await db.select().from("users");
+        const users = getUserData();
+        res.send(users);
+      })
+    );
+
+    const sleep = () =>
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("ok");
+        }, 1000);
+      });
+
+    server.get(
+      "/continue",
+      asyncHandler(async (req, res, next) => {
+        await sleep();
+        console.log("hello");
+        next();
+      }),
+      asyncHandler(async (req, res, next) => {
+        await sleep();
+        console.log("world");
+        next();
+      }),
+      asyncHandler(async (req, res, next) => {
+        await sleep();
+        console.log("express");
+        res.send("javascript");
+      })
+    );
 
     // その他はすべてNextのrouterに飛ばす
     server.get("*", (req, res) => {
---
+++ server/util/getUser.js
@@ -0,0 +1,9 @@
+const data = [
+  { name: "tanaka", mail: "tanaka@gmail.com" },
+  { name: "yamada", mail: "yamada@gmail.com" },
+  { name: "suzuki", mail: "suzuki@gmail.com" }
+];
+
+const getUserData = () => data;
+
+export default getUserData;
