5aa8a918a6972089a096a251fbb592963bc838be
yoneda <nsshowdream@gmail.com>
2020-03-08 21:10:06 +0900

    [add] add knex migrations

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
     client: "sqlite3",
     connection: {
       filename: "./dev.sqlite3"
+    },
+    migrations: {
+      directory: "server/migrations"
     }
   },
 
---
+++ server/migrations/20200308180816_init.js
@@ -0,0 +1,23 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", table => {
+      table.increments("id");
+      table.string("mail");
+      table.string("pass");
+      table.string("account");
+      table.boolean("showStatus");
+      table.boolean("showCalendar");
+    })
+    .createTable("notes", table => {
+      table.increments("id");
+      table.string("title");
+      table.string("body");
+      table.datetime("createdAt");
+      table.datetime("updatedAt");
+      table.integer("user");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable("users").dropTable("notes");
+};
