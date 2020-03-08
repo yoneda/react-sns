6a7252134f3d91d388a93f8fb4a24660954192f7
yoneda <nsshowdream@gmail.com>
2020-03-08 18:05:23 +0900

    [add] add fake.js

---
+++ fake.js
@@ -0,0 +1,55 @@
+const dayjs = require("dayjs");
+
+const randDatetime = () =>
+  dayjs()
+    .subtract(Math.floor(Math.random() * 60), "day")
+    .format("YYYY-M-D H:mm:ss");
+
+const fake = {
+  users: [
+    {
+      id: 1,
+      mail: "yoneda@yoneda.com",
+      pass: "yonedapass",
+      account: "yoneda",
+      showStatus: true,
+      showClaendar: true
+    },
+    {
+      id: 2,
+      mail: "tanaka@tanaka.com",
+      pass: "tanakapass",
+      account: "tanaka",
+      showStatus: false,
+      showClaendar: false
+    }
+  ],
+  notes: [
+    {
+      id: 1,
+      title: "test note",
+      body: "text text text",
+      createdAt: randDatetime(),
+      updatedAt: randDatetime(),
+      userId: 1
+    },
+    {
+      id: 2,
+      title: "test note 2",
+      body: "text text text 2",
+      createdAt: randDatetime(),
+      updatedAt: randDatetime(),
+      userId: 1
+    },
+    {
+      id: 3,
+      title: "test note",
+      body: "text text text",
+      createdAt: randDatetime(),
+      updatedAt: randDatetime(),
+      userId: 2
+    }
+  ]
+};
+
+module.exports = fake;
