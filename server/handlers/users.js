bab6b6156cd05978f1edd382e25adc0e1e970c89
yoneda <nsshowdream@gmail.com>
2020-03-09 23:08:34 +0900

    [add] add new endpoint case of PUT /api/users/:account

--- server/handlers/users.js
+++ server/handlers/users.js
@@ -1,8 +1,12 @@
 const db = require("../db");
+import { pick } from "lodash";
 
 export const get = async (req, res) => {
   const { account } = req.params;
   const user = await db("users").where({ account: account });
+  if (user && user.length === 0) {
+    res.status(404).send({ message: "not found" });
+  }
   res.send(user);
 };
 
@@ -24,6 +28,19 @@ export const post = async (req, res) => {
     .send(user);
   // TODO: POSTではlocationヘッダに作成後のURLを含めることが推奨されている。
   // TODO: POSTでは作成された情報を返すことが推奨されている。Postgreでは1回のクエリで作成情報が返るが SQLiteでは2回必要。
+};
+
+export const put = async (req, res) => {
+  const { account } = req.params;
+  const query = pick(req.body, ["mail", "pass", "showStatus", "showCalendar"]);
+  const id = await db("users")
+    .where({ account })
+    .update(query);
+  const user = await db("users").where({ id });
+  res.send(user);
+  // TODO: メールアドレスも変更できるように修正
+  // TODO: アカウント名も変更できるか検討
+  // TODO: バリデーション実装が必要か検討
 };
 
 export const remove = async (req, res) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -8,6 +8,8 @@ router.get("/:account", asyncHandler(users.get));
 
 router.post("/", asyncHandler(users.post));
 
+router.put("/:account", asyncHandler(users.put));
+
 router.delete("/:account", asyncHandler(users.remove));
 
 export default router;
