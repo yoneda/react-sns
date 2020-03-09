0b2446353bd13b4a7e66696ba7a080e43d62482d
yoneda <nsshowdream@gmail.com>
2020-03-09 23:32:23 +0900

    [clean] adjust indention

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,11 +5,8 @@ import * as users from "../handlers/users";
 const router = Router();
 
 router.get("/:account", asyncHandler(users.get));
-
 router.post("/", asyncHandler(users.post));
-
 router.put("/:account", asyncHandler(users.put));
-
 router.delete("/:account", asyncHandler(users.remove));
 
 export default router;
