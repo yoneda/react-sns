7e99696788e73e5cc07e4da0f280f40c648c3a2f
yoneda <nsshowdream@gmail.com>
2020-03-10 10:43:43 +0900

    [add] add post, put interface to update notes

--- server/handlers/notes.js
+++ server/handlers/notes.js
@@ -3,6 +3,7 @@ import { pick } from "lodash";
 
 export const get = async (req, res) => {
   const { account } = req.query;
+  // MEMO: ここは後で認証済みのuserオブジェクトから取得する予定
   const notes = await db("notes")
     .join("users", "users.id", "notes.user")
     .where("users.account", account)
@@ -17,6 +18,15 @@ export const get = async (req, res) => {
   res.send(notes);
 };
 
+export const post = async(req, res) => {
+  const { account } = req.query;
+  
+}
+
+export const put = async(req, res) => {
+  
+}
+
 export const remove = async (req, res) => {
   const { id } = req.params;
   const num = await db("notes")
@@ -24,6 +34,7 @@ export const remove = async (req, res) => {
     .del();
   if (!num) {
     throw new Error("Note is not found");
+  }
     // TODO: エラー時の status code が500で間違いないかチェックする
   res.status(200).send({ message: "Note has been deleted successfully." });
-};
+}
