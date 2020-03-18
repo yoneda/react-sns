d310c57600bd6b45327da25b77a6c6233f0db047
yoneda <nsshowdream@gmail.com>
2020-03-18 12:55:44 +0900

    [fix] implement login, register static pages

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from "../components/Header";
 
-const Login = props => {
-    const { num, name } = props;
-    return (
-        <div>
-          <Header />
-          login
-        </div>
-    );
+const Register = props => {
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+
+  return (
+    <div>
+      <Header />
+      <h3>login:</h3>
+      <input
+        type="text"
+        onChange={e => setMail(e.target.value)}
+        value={mail}
+        placeholder="mail"
+      />
+      <br />
+      <input
+        type="text"
+        onChange={e => setPass(e.target.value)}
+        value={pass}
+        placeholder="pass"
+      />
+      <br />
+      <button onClick={() => {}}>send</button>
+    </div>
+  );
 };
 
-export default Login;
+export default Register;
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 import Header from "../components/Header";
 
 const Register = props => {
-    const { num, name } = props;
-    return (
-        <div>
-          <Header />
-          register
-        </div>
-    );
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+  const [account, setAccount] = useState("");
+
+  return (
+    <div>
+      <Header />
+      <h3>register:</h3>
+      <input
+        type="text"
+        onChange={e => setMail(e.target.value)}
+        value={mail}
+        placeholder="mail"
+      />
+      <br />
+      <input
+        type="text"
+        onChange={e => setPass(e.target.value)}
+        value={pass}
+        placeholder="pass"
+      />
+      <br />
+      <input
+        type="text"
+        onChange={e => setAccount(e.target.value)}
+        value={account}
+        placeholder="account"
+      />
+      <br />
+      <button onClick={() => {}}>send</button>
+    </div>
+  );
 };
 
 export default Register;
