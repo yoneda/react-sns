40e434b927c93a1684b52b01126f273daac0e700
yoneda <nsshowdream@gmail.com>
2020-03-24 22:07:01 +0900

    [add] add api request agent

---
+++ client/agent.js
@@ -0,0 +1,19 @@
+import request from "superagent";
+
+// APIとのI/Oなのでいち速くTS化したい
+
+const Note = {
+  get: () => {},
+  post: () => {},
+  put: () => {}
+};
+
+const User = {
+  get: () => {},
+  put: () => {}
+};
+
+export default {
+  Note,
+  User
+};
