71769d8b447553e9ba670bf83b80997752c6697e
yoneda <nsshowdream@gmail.com>
2020-04-07 23:13:03 +0900

    [fix] disbled <Menu />

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,20 +1,19 @@
 import React from "react";
 import Link from "next/link";
 
-const Header = props => {
+const Header = (props) => {
   const { children } = props;
-  return(
+  return (
     <div>
-      <Link href="/"><a><h2>Simple Diary</h2></a></Link>{children}<br />
-      <div>
-        <Link href="/"><a>Top</a></Link> |
-        <Link href="/new"><a itemProp="hello, world">New</a></Link> |
-        <Link href="/setting"><a>Setting</a></Link> |
-        <Link href="/register"><a>Register</a></Link> |
-        <Link href="/login"><a>Login</a></Link>
-      </div>
+      <Link href="/">
+        <a>
+          <h2>Simple Diary</h2>
+        </a>
+      </Link>
+      {children}
+      <br />
     </div>
-  )
-}
+  );
+};
 
 export default Header;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { StoreProvider } from "easy-peasy";
 import store from "../store";
 import Header from "../components/Header";
 import EditorModal from "../components/EditorModal";
 import dayjs from "dayjs";
+import Link from "next/link";
+
+const Menu = () => (
+  <Fragment>
+    <Link href="/"><a>Top</a></Link> |
+    <Link href="/new"><a>New</a></Link> |
+    <Link href="/setting"><a>Setting</a></Link> |
+    <Link href="/register"><a>Register</a></Link> |
+    <Link href="/login"><a>Login</a></Link>
+  </Fragment>
+)
 
 const MyApp = ({ Component, pageProps }) => {
   const createNote = store.getActions().notes.create;
   const [open, setOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const today = dayjs().format("YYYY-M-D");
   return (
     <div>
       <Header>
         <button onClick={() => setOpen(!open)}>add</button>
-        <button onClick={() => setOpen(!open)}>menu</button>
+        <button onClick={() => setMenuOpen(!menuOpen)}>menu</button>
       </Header>
+      {
+        menuOpen && <Menu />
+      }
       {open && (
         <EditorModal
           datetime={today}
