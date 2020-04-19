1c846587971f4154de76ea24a4fc9bd34d841894
yoneda <nsshowdream@gmail.com>
2020-04-20 01:42:13 +0900

    [fix] make it possible to authenticate before database connection

--- server/handlers/notes.js
+++ server/handlers/notes.js
@@ -3,11 +3,11 @@ const dayjs = require("dayjs");
 const db = require("../db");
 
 module.exports.get = async (req, res) => {
-  const { account } = req.query;
+  const mail = req.mail;
   // MEMO: ここは後で認証済みのuserオブジェクトから取得する予定
   const notes = await db("notes")
     .join("users", "users.id", "notes.user")
-    .where("users.account", account)
+    .where("users.mail", mail)
     .select(
       "notes.id",
       "notes.body",
@@ -19,44 +19,38 @@ module.exports.get = async (req, res) => {
 };
 
 module.exports.post = async (req, res) => {
-  const { account,  body } = req.body;
-  const [{ id: userId }] = await db("users")
-    .select("id")
-    .where({ account });
+  const mail = req.mail;
+  const { body } = req.body;
+  const [{ id: userId }] = await db("users").select("id").where({ mail });
   const today = dayjs().format("YYYY-M-D H:mm:ss");
   const [id] = await db("notes").insert({
     body,
     createdAt: today,
     updatedAt: today,
-    user: userId
+    user: userId,
   });
-  const [note] = await db("notes")
-    .where({ id })
-    .limit(1);
-  res
-    .status(201)
-    .location("location")
-    .send(note);
+  const [note] = await db("notes").where({ id }).limit(1);
+  res.status(201).location("location").send(note);
   // TODO: POSTではlocationヘッダに作成後のURLを含めることが推奨されている。
   // TODO: POSTでは作成された情報を返すことが推奨されている。Postgreでは1回のクエリで作成情報が返るが SQLiteでは2回必要。
 };
 
 module.exports.put = async (req, res) => {
+  const mail = req.mail;
   const { id } = req.params;
   const payload = pick(req.body, ["body"]);
   const today = dayjs().format("YYYY-M-D H:mm:ss");
   await db("notes")
-    .where({ id })
+    .where({ id, mail })
     .update({ ...payload, updatedAt: today });
   const [note] = await db("notes").where({ id });
   res.send(note);
 };
 
 module.exports.remove = async (req, res) => {
+  const mail = req.mail;
   const { id } = req.params;
-  const num = await db("notes")
-    .where({ id })
-    .del();
+  const num = await db("notes").where({ id, mail }).del();
   if (!num) {
     throw new Error("Note is not found");
   }
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const asyncHandler = require("express-async-handler");
 const notes = require("../handlers/notes");
+const auth = require("../handlers/auth");
 
 const router = Router();
 
-router.get("/", asyncHandler(notes.get));
-router.post("/", asyncHandler(notes.post));
-router.put("/:id", asyncHandler(notes.put));
-router.delete("/:id", asyncHandler(notes.remove));
+router.get("/", asyncHandler(auth), asyncHandler(notes.get));
+router.post("/", asyncHandler(auth), asyncHandler(notes.post));
+router.put("/:id", asyncHandler(auth), asyncHandler(notes.put));
+router.delete("/:id", asyncHandler(auth), asyncHandler(notes.remove));
 
 module.exports = router;
