b8e4bc004da6774fa136ded11fdb129593cecba7
yoneda <nsshowdream@gmail.com>
2020-04-20 20:47:19 +0900

    [fix] some refactoring in agent.js

--- client/agent.js
+++ client/agent.js
@@ -4,24 +4,24 @@ import request from "superagent";
 
 const base = "http://localhost:3000/api";
 
-const Helth = {
-  get: () => request.get(`${base}/helth`),
-};
+const CheckHelth = () => request.get(`${base}/helth`);
 
+const CheckAuth = () =>
+  request.get(`${base}/checkAuth`).then((res) => res.status === 200);
+
+// TODO: typescriptで返り値の型を指定したい
 const Note = {
-  get: () => request.get(`${base}/notes`).then((res) => res.body), // TODO: ここで返り値の型を指定したいところ
-  post: (params) =>
+  get: () => request.get(`${base}/notes`).then((res) => res.body),
+  post: ({ body }) =>
     request
       .post(`${base}/notes`)
-      .send(params)
-      .then((res) => res.body), // TODO: ここで返り値の型を指定したいところ
-  put: (params) => {
-    const { id, body } = params;
-    return request
-      .put(`${base}/notes/${id}`)
       .send({ body })
-      .then((res) => res.body); // TODO: ここで返り値の型を指定したいところ
-  },
+      .then((res) => res.body),
+  put: ({ id, body }) =>
+    request
+      .put(`${base}/notes/${id}`)
+      .send({ id, body })
+      .then((res) => res.body),
 };
 
 const User = {
@@ -31,33 +31,23 @@ const User = {
       .post(`${base}/users`)
       .send({ mail, pass })
       .then((res) => res.body),
-  put: (params) => {
-    const { pass, bio, showCalendar, showDateEditor, calendarStart } = params;
-    return request
+  put: ({ pass, bio, showCalendar, showDateEditor, calendarStart }) =>
+    request
       .put(`${base}/users`)
       .send({ pass, bio, showCalendar, showDateEditor, calendarStart })
-      .then((res) => res.body[0]);
-  },
-  login: function (params) {
-    const { mail, pass } = params;
-    return request
+      .then((res) => res.body[0]),
+  login: ({ mail, pass }) =>
+    request
       .post(`${base}/users/login`)
       .send({ mail, pass })
-      .then((res) => res.status === 200);
-  },
+      .then((res) => res.status === 200),
   logout: () =>
     request.post(`${base}/users/logout`).then((res) => res.status === 200),
 };
 
-const CheckAuth = () =>
-  request
-    .get(`${base}/checkAuth`)
-    .then((res) => res.status === 200)
-    .catch((err) => false);
-
 export default {
-  Helth,
+  CheckHelth,
+  CheckAuth,
   Note,
   User,
-  CheckAuth,
 };
