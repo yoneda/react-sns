18de819977bb0ea73952695ba48c72eb76d98515
yoneda <nsshowdream@gmail.com>
2020-06-18 16:07:36 +0900

    [fix] add Login

--- client/pages/login2.js
+++ client/pages/login2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "@reach/router";
 
 const Box = styled.div`
   display: flex;
@@ -11,11 +12,41 @@ const Item = styled.div`
   width: 400px;
 `;
 
+const Input = styled.input`
+  width: 400px;
+  box-sizing: border-box;
+  margin-top: 10px;
+`;
+
+const Button = styled.button`
+  width: 400px;
+  margin-top: 10px;
+`;
+
+const Header = styled.div`
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function Login2() {
   return (
     <Box>
       <Item>
-        login
+        <Header>
+          <h3>Sign in</h3>
+        </Header>
+        <Input type="text" placeholder="メールアドレス" />
+        <Input type="text" placeholder="パスワード" />
+        <Button>ログインする</Button>
+        <div>または</div>
+        <Button>Googleで続ける</Button>
+        <div>
+          すでにアカウントをお持ちですか？
+          <br />
+          お持ちの場合は<Link to="../signup">こちら</Link>から登録できます。
+        </div>
       </Item>
     </Box>
   );
