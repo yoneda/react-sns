2946772b945c56aa28b844e33f9a846af00948c5
yoneda <nsshowdream@gmail.com>
2020-06-30 13:44:42 +0900

    [fix] refactoring index.js

--- client/index.js
+++ client/index.js
@@ -1,22 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import agent from "./agent";
 import App from "./pages/app";
-
-function HelloMessage(props) {
-  const { name } = props;
-  const [value, setValue] = useState("");
-  useEffect(()=>{
-    agent.Helth.get().then(res=>{
-      const val = JSON.parse(res.text);
-      setValue(val.val);
-    })
-  },[]);
-  return (
-    <div>
-      hello, {name}, responce value is {value}
-    </div>
-  );
-}
 
 ReactDOM.render(<App />, document.getElementById("app"));
