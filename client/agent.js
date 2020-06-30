66cab6cf7859a420e63d20513225986df7ffa95f
yoneda <nsshowdream@gmail.com>
2020-06-30 13:41:17 +0900

    [clean] refactoring agent.js

--- client/agent.js
+++ client/agent.js
@@ -4,91 +4,49 @@ import { pick } from "lodash";
 const port = process.env.PORT || 3000;
 const base = `http://localhost:${port}/api`;
 
-/*
-// TODO: typescriptで返り値の型を指定したい
-const Note = {
-  get: () => request.get(`${base}/notes`).then((res) => res.body),
-  post: ({ body }) =>
-    request
-      .post(`${base}/notes`)
-      .send({ body })
-      .then((res) => res.body),
-  put: ({ id, body }) =>
-    request
-      .put(`${base}/notes/${id}`)
-      .send({ id, body })
-      .then((res) => res.body),
+const CheckHelth = function () {
+  return request.get(`${base}/helth`);
 };
 
-const User = {
-  get: () => request.get(`${base}/users`).then((res) => res.body[0]),
-  post: function ({ email, password }) {
-    return request
-      .post(`${base}/users`)
-      .send({ user: { email, password } })
-      .then((res) => res.bpdy);
-  },
-  put: ({ password, bio, showCalendar, showDateEditor, calendarStart }) =>
-    request
-      .put(`${base}/users`)
-      .send({ password, bio, showCalendar, showDateEditor, calendarStart })
-      .then((res) => res.body[0]),
-  login: ({ email, password }) =>
-    request
-      .post(`${base}/users/login`)
-      .send({ user: { email, password } })
-      .then((res) => res.status === 200),
-  logout: () =>
-    request.post(`${base}/users/logout`).then((res) => res.status === 200),
-};
-
-const CheckAuth = () =>
-  request
+const CheckAuth = function () {
+  return request
     .get(`${base}/checkAuth`)
     .then((res) => res.status === 200)
     .catch(() => false);
-
-const CheckHelth = () => request.get(`${base}/helth`);
-
-export default {
-  Note,
-  User,
-  CheckAuth,
-  CheckHelth,
 };
-*/
-
-const CheckAuth = () =>
-  request
-    .get(`${base}/checkAuth`)
-    .then((res) => res.status === 200)
-    .catch(() => false);
 
 const Note = {
-  get: (reqQuery) => {
+  get: function (reqQuery) {
     const query = pick(reqQuery, ["trashed", "limit"]);
     return request
       .get(`${base}/notes`)
       .query(query)
       .then((res) => res.body.notes);
   },
-  put: ({ id, reqBody }) =>
-    request
+  put: function ({ id, reqBody }) {
+    return request
       .put(`${base}/notes/${id}`)
       .send(reqBody)
-      .then((res) => res.body),
-  post: ({ reqBody }) =>
-    request
+      .then((res) => res.body);
+  },
+  post: function ({ reqBody }) {
+    return request
       .post(`${base}/notes`)
       .send(reqBody)
-      .then((res) => res.body),
-  remove: ({ id }) =>
-    request.delete(`${base}/notes/${id}`).then((res) => res.body),
-  trash: ({ id }) =>
-    request.put(`${base}/notes/${id}/trash`).then((res) => res.body),
-  restore: ({ id }) =>
-    request.put(`${base}/notes/${id}/restore`).then((res) => res.body),
-  tidyGarbage: () => request.delete(`${base}/notes/garbage`),
+      .then((res) => res.body);
+  },
+  remove: function ({ id }) {
+    return request.delete(`${base}/notes/${id}`).then((res) => res.body);
+  },
+  trash: function ({ id }) {
+    return request.put(`${base}/notes/${id}/trash`).then((res) => res.body);
+  },
+  restore: function ({ id }) {
+    return request.put(`${base}/notes/${id}/restore`).then((res) => res.body);
+  },
+  tidyGarbage: function () {
+    return request.delete(`${base}/notes/garbage`);
+  },
 };
 
 const User = {
@@ -121,10 +79,6 @@ const User = {
   delete: function () {
     return request.delete(`${base}/users`).then((res) => res.body);
   },
-};
-
-const CheckHelth = function () {
-  return request.get(`${base}/helth`);
 };
 
 module.exports = {
