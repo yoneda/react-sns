5b88ced394242136220e8f282817922f9a0482ea
yoneda <nsshowdream@gmail.com>
2020-06-30 15:07:19 +0900

    [clean] remove unused files

--- server/seeds/notes.js
+++
@@ -1,6 +0,0 @@
-import fake from "../../fake";
-
-exports.seed = knex =>
-  knex("notes")
-    .del()
-    .then(() => knex("notes").insert(fake.notes));
--- server/util/getUser.js
+++
@@ -1,9 +0,0 @@
-const data = [
-  { name: "tanaka", mail: "tanaka@gmail.com" },
-  { name: "yamada", mail: "yamada@gmail.com" },
-  { name: "suzuki", mail: "suzuki@gmail.com" }
-];
-
-const getUserData = () => data;
-
-export default getUserData;
