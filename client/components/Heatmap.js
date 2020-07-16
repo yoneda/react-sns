f90db478b2b180033a6d7ddad1ec7d1d602abbd4
yoneda <nsshowdream@gmail.com>
2020-07-16 17:04:24 +0900

    [fix] add Heatmap Component

---
+++ client/components/Heatmap.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: skyblue;
+`;
+
+function Heatmap(props) {
+  return <Box></Box>;
+}
+
+export default Heatmap;
---
+++ stories/heatmap.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Heatmap from "../client/components/Heatmap";
+
+export default {
+  title: "Heatmap",
+};
+export const normal = () => <Heatmap />;
