9a14790bd1356bac93a52c724c05b46d4f01cd30
yoneda <nsshowdream@gmail.com>
2020-09-15 16:43:05 +0900

    [fix] fix to generate data suitable for debugging Heatmap

--- server/seeds/random.js
+++ server/seeds/random.js
@@ -2,14 +2,14 @@ import faker from "faker";
 import dayjs from "dayjs";
 import bcrypt from "bcrypt";
 
-const NUM_USERS = 10;
-const NUM_NOTES = 365;
+const NUM_USERS = 3;
+const NUM_NOTES = 10;
 
 // TODO: 時刻までランダムにする
 // TODO: ランダムの性能を高める
 const randDatetime = function () {
   return dayjs()
-    .subtract(Math.floor(Math.random() * 60), "day")
+    .subtract(Math.floor(Math.random() * 10), "day")
     .format("YYYY-M-D H:mm:ss");
 };
 
@@ -32,7 +32,7 @@ const seed = async function (knex) {
     for (let noteIndex = 0; noteIndex < NUM_NOTES; noteIndex++) {
       await knex("notes").insert({
         title: faker.lorem.words(),
-        body: faker.lorem.sentences(),
+        body: faker.lorem.words(),
         trashed: false,
         createdAt: randDatetime(),
         updatedAt: randDatetime(),
