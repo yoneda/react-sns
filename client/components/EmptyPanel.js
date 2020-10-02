eb5bb939935d426a76a780f4936dac3aef480f85
yoneda <nsshowdream@gmail.com>
2020-10-02 17:22:34 +0900

    [fix] add EmptyPanel

---
+++ client/components/EmptyPanel.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Box = styled.div`
+  background: whitesmoke;
+  height: 100%;
+  width: 100%;
+`;
+
+function EmptyPanel(props) {
+  return <Box>aaa</Box>;
+}
+export default EmptyPanel;
