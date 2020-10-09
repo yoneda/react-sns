5a0100ee73fc07a2708a75bf9b163b159115ead7
yoneda <nsshowdream@gmail.com>
2020-10-09 14:47:46 +0900

    [clear] remove unused layout

--- client/components/Main.js
+++ client/components/Main.js
@@ -7,8 +7,14 @@ import EditPane from "./EditPane";
 import Heatmap from "./Heatmap";
 import Modal from "./Modal";
 import SettingPanel from "./SettingPanel";
-import Empty from "./Empty";
-import { isEmpty } from "lodash";
+
+const Draggable = styled.div`
+  grid-area: drag;
+  border-bottom: 1px solid darkgray;
+  height: 40px;
+  width: 100%;
+  -webkit-app-region: drag;
+`;
 
 function DeleteModal(props) {
   const { position } = props;
@@ -72,7 +78,7 @@ function Modals() {
   return (
     <Fragment>
       {modals.map((modal, index) => {
-        const position = { x: (index + 1) * 10, y: (index + 1) * 10 + 40 };
+        const position = { x: (index + 1) * 10, y: (index + 1) * 10 };
         if (modal === "SETTING_PANEL") {
           return (
             <Modal
@@ -96,72 +102,7 @@ function Modals() {
   );
 }
 
-const Draggable = styled.div`
-  grid-area: drag;
-  border-bottom: 1px solid darkgray;
-  height: 40px;
-  width: 100%;
-  -webkit-app-region: drag;
-`;
-
-const Layout = styled.div`
-  display: grid;
-  height: 100vh;
-  grid-template-columns: 200px 250px 1fr;
-  grid-template-rows: 40px 1fr 1fr 60px;
-  grid-template-areas:
-    "drag drag drag"
-    "menu heatmap edit"
-    "menu list edit"
-    "new list edit";
-`;
-
-const MenuBox = styled.div`
-  grid-area: menu;
-`;
-
-const HeatmapBox = styled.div`
-  grid-area: heatmap;
-`;
-
-const EditBox = styled.div`
-  grid-area: edit;
-  background-color: lightpink;
-`;
-
-const ListBox = styled.div`
-  grid-area: list;
-`;
-
-const NewBox = styled.div`
-  grid-area: new;
-`;
-
-function Main(props) {
-  const user = useStoreState((state) => state.app.user);
-  const focus = useStoreState((state) => state.notes.focus);
-  return (
-    <Fragment>
-      <Modals />
-      <Layout>
-        <Draggable />
-        <MenuBox>
-          <MenuPane />
-        </MenuBox>
-        <NewBox></NewBox>
-        <HeatmapBox>
-          <Heatmap />
-        </HeatmapBox>
-        <ListBox>
-          <ListPane />
-        </ListBox>
-        <EditBox>{isEmpty(focus) ? <Empty /> : <EditPane />}</EditBox>
-      </Layout>
-    </Fragment>
-  );
-}
-
-function Main2(props) {
+function Main() {
   return (
     <Fragment>
       <Modals />
@@ -174,4 +115,4 @@ function Main2(props) {
   );
 }
 
-export default Main2;
+export default Main;
